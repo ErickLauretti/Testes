923771d9ce443240c6624580a7ec7b5befedff65
Erick Lauretti <erick.nighyt@hotmail.com>
2022-09-28 16:37:09 -0300

    requisito 5

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,6 @@ const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
     return undefined;
   }
-    // eslint-disable-next-line max-len
     return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
